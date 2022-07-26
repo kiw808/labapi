2757fe4e41ece14a1793aa0995d8e7f13328b7e3
Pierre Muller <pierremanu.muller@gmail.com>
2022-07-26 21:30:51 +0200

    feat(app): basic express server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('Hello world')
+import express, {Express, Request, Response} from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app: Express = express()
+const port = process.env.APP_PORT || "3001"
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world')
+})
+
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}`)
+})
