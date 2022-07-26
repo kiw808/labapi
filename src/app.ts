011e13845fe0d10b2c939f970f4dd7586ec96f2c
Pierre Muller <pierremanu.muller@gmail.com>
2022-07-26 23:58:30 +0200

    feat(app): basic api

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,19 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.APP_PORT || '3001';
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello world');
+// Middlewares
+app.use(express.json());
+
+// Routes
+app.get('/', (req: Request, res: Response) => res.send('Welcome on LabAPI'));
+
+app.get('/api/books/:bookId', (req: Request, res: Response) => {
+  console.log(req.params);
+
+  return res.send(req.params);
 });
 
+// Start listening
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
 });
